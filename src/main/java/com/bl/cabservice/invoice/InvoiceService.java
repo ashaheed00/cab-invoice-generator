2067478c0d752945c62ce622497ee05e0c43e91c
ashaheed00 <ashaheed00@gmail.com>
2020-10-20 14:02:05 +0530

    Step 5: Refactored to remove obsolete parts from InvoiceService and Test

--- src/main/java/com/bl/cabservice/invoice/InvoiceService.java
+++ src/main/java/com/bl/cabservice/invoice/InvoiceService.java
@@ -4,25 +4,16 @@ import com.bl.cabservice.rides.Ride;
 import com.bl.cabservice.rides.RideRepository;
 
 public class InvoiceService {
-	private final double COST_PER_KM = 10;
-	private final double COST_PER_MINUTE = 1;
-	private final double MINIMUM_FARE = 5;
 	private RideRepository rideRepository;
 
 	public void setRideRepository(RideRepository rideRepository) {
 		this.rideRepository = rideRepository;
 	}
 
-	public double calculateFarePerRide(double distance, int timeInMinute) {
-		double fare = distance * COST_PER_KM + timeInMinute * COST_PER_MINUTE;
-		return fare < 5 ? MINIMUM_FARE : fare;
-	}
-
 	public InvoiceSummary calculateFare(Ride[] rides) {
 		double totalFare = 0;
 		for (Ride ride : rides)
-			totalFare += ride.cabRideType.calculateFarePerRide(ride.distance, ride.timeInMinute); // calculateFarePerRide(ride.distance,
-																									// ride.timeInMinute);
+			totalFare += ride.cabRideType.calculateFarePerRide(ride.distance, ride.timeInMinute);
 		return new InvoiceSummary(rides.length, totalFare);
 	}
 
--- src/test/java/com/bl/cabservice/invoice/InvoiceServiceTest.java
+++ src/test/java/com/bl/cabservice/invoice/InvoiceServiceTest.java
@@ -12,28 +12,22 @@ import com.bl.cabservice.rides.RideRepository;
 public class InvoiceServiceTest {
 	InvoiceService invoiceService;
 	RideRepository rideRepository;
+	String userId;
+	Ride[] rides;
 
 	@Before
 	public void initializer() {
 		invoiceService = new InvoiceService();
 		rideRepository = new RideRepository();
-	}
-
-	@Test
-	public void givenDistanceTimeShouldReturnTotalFare() {
-		double fare = invoiceService.calculateFarePerRide(2.5, 5);
-		assertEquals(30, fare, 0);
-		fare = invoiceService.calculateFarePerRide(0.2, 1);
-		assertEquals(5, fare, 0);
-	}
-
-	@Test
-	public void givenUserIdAndRides_ForNormaAndPremimum_ShouldReturnInvoiceSummary() {
-		String userId = "user@id.com";
+		userId = "user@id.com";
 		Ride[] rides = { new Ride(CabRide.PREMIUM, 2, 5), new Ride(CabRide.NORMAL, 0.2, 1),
 				new Ride(CabRide.NORMAL, 5, 5) };
 		invoiceService.setRideRepository(rideRepository);
 		invoiceService.addRides(userId, rides);
+	}
+
+	@Test
+	public void givenUserIdAndRides_ForNormaAndPremimum_ShouldReturnInvoiceSummary() {
 		InvoiceSummary actualInvoiceSummary = invoiceService.getInvoiceSummary(userId);
 		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(3, 100);
 		assertEquals(expectedInvoiceSummary, actualInvoiceSummary);
